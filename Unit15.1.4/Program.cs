3c1974b290f7e786c5c9dc8f93d976b8f0da4ea5
Mellomanprost <anoppa@mail.ru>
2022-11-25 12:16:54 +0300

    Add project files.

---
+++ Unit15.1.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Unit15._1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountCommonChar("individual", "responsoble");
+        }
+
+        static void CountCommonChar(string word1, string word2)
+        {
+            var commonChar = word1.Intersect(word2);
+            foreach (var item in commonChar)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
