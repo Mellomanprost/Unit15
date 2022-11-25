417f414e20c2adbb763c6406c5b7b11f1cafd148
Mellomanprost <anoppa@mail.ru>
2022-11-25 12:18:56 +0300

    Added exercise 15.1.5

---
+++ Unit15.1.5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Unit15._1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var softwareManufacturers = new List<string>()
+            {
+                "Microsoft", "Apple", "Oracle"
+            };
+
+            var hardwareManufacturers = new List<string>()
+            {
+                "Apple", "Samsung", "Intel"
+            };
+
+            var itCompanies = softwareManufacturers.Union(hardwareManufacturers);
+        }
+    }
+}
